40e55763e7ecc860e8de46157fa47fc1fe5b25c3
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-09-15 01:30:35 +0500

    service section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
+import Services from "./Components/Services";
 
 function App() {
   return (
     <div>
       <Header />
       <Hero />
+      <Services />
     </div>
   );
 }
